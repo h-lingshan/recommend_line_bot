6acb12f35c7261fe2dfac9cf871470d7dda077b6
lingshan.hsu <lingshan.hsu@nijibox.co.jp>
2017-07-07 18:58:51 +0900

    teste

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -39,19 +39,3 @@
   end
 end
 
-module Line
-  module Bot
-    class HTTPClient
-      def http(uri)
-        proxy = URI(ENV["FIXIE_URL"])
-        http = Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
-        if uri.scheme == "https"
-          http.use_ssl = true
-        end
-
-        http
-      end
-    end
-  end
-end
-
