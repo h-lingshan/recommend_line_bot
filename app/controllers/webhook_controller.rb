e04f961b1857d6c1fd297f08ab3d034f2527df43
lingshan.hsu <lingshan.hsu@nijibox.co.jp>
2017-07-07 19:30:57 +0900

    teste

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,44 +1,8 @@
- #require 'line/bot'
- 
- class WebhookController < ApplicationController
+class WebhookController < ApplicationController
+  # Lineからのcallbackか認証
   protect_from_forgery with: :null_session
 
-
-#   def callback
-#     body = request.body.read
-
-#     signature = request.env['HTTP_X_LINE_SIGNATURE']
-#     unless client.validate_signature(body, signature)
-#       error 400 do 'Bad Request' end
-#     end
-
-#     events = client.parse_events_from(body)
-#     events.each { |event|
-#       case event
-#       when Line::Bot::Event::Message
-#         case event.type
-#         when Line::Bot::Event::MessageType::Text
-#           message = {
-#             type: 'text',
-#             text: event.message['text']
-#           }
-#           response = client.reply_message(event['replyToken'], message)
-#           p response
-#         end
-#       end
-#     }
-#     render status: 200, json: { message: 'OK' }
-#   end
-
-#   private
-#   def client
-#     @client ||= Line::Bot::Client.new { |config|
-#       config.channel_secret = ENV["CHANNEL_SECRET"]
-#       config.channel_token = ENV["CHANNEL_ACCESS_TOKEN"]
-#     }
-#   end
-
-CHANNEL_SECRET = ENV['CHANNEL_SECRET']
+  CHANNEL_SECRET = ENV['CHANNEL_SECRET']
   OUTBOUND_PROXY = ENV['OUTBOUND_PROXY']
   CHANNEL_ACCESS_TOKEN = ENV['CHANNEL_ACCESS_TOKEN']
 
@@ -68,6 +32,7 @@ CHANNEL_SECRET = ENV['CHANNEL_SECRET']
 
     render :nothing => true, status: :ok
   end
+
   private
   # verify access from LINE
   def is_validate_signature
@@ -78,4 +43,3 @@ CHANNEL_SECRET = ENV['CHANNEL_SECRET']
     signature == signature_answer
   end
 end
-
