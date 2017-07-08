41f6eeadc8bd9725cc1f8232d0dee9cf1fa70e93
ReeceHsu <viewsoic521@gmail.com>
2017-07-08 21:10:13 +0900

    123

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,63 +1,59 @@
-class WebhookController < ApplicationController
-  
-  protect_from_forgery :except => [:callback]
- 
 require 'line/bot'
-require 'net/http'
- 
-def client
-       client = Line::Bot::Client.new { |config|
- config.channel_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
- config.channel_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
- }
-end
- 
- 
- 
-def callback
- 
-  body = request.body.read
- 
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
- 
-  event = params["events"][0]
-  event_type = event["type"]
- 
-  #送られたテキストメッセージをinput_textに取得
-  input_text = event["message"]["text"]
- 
-  events = client.parse_events_from(body)
- 
-  events.each { |event|
- 
-    case event
+
+class WebhookController < ApplicationController
+  protect_from_forgery with: :null_session # CSRF対策無効化
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    # this statement maybe mistake.
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
       when Line::Bot::Event::Message
         case event.type
-          #テキストメッセージが送られた場合、そのままおうむ返しする
-          when Line::Bot::Event::MessageType::Text
-             message = {
-                  type: 'text',
-                  text: input_text
-                  }
- 
-          #画像が送られた場合、適当な画像を送り返す
-          #画像を返すには、画像が保存されたURLを指定する。
-          #なお、おうむ返しするには、１度AWSなど外部に保存する必要がある。ここでは割愛する
-          when Line::Bot::Event::MessageType::Image
-            image_url = "https://XXXXXXXXXX/XXX.jpg"  #httpsであること
-              message = {
-                  type: "image",
-                  originalContentUrl: image_url,
-                  previewImageUrl: image_url
-                  }
-         end #event.type
-         #メッセージを返す
-         client.reply_message(event['replyToken'],message)
-    end #event
- } #events.each
- render :nothing => true, status: :ok
- 
-end  #def
- 
- 
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          response = client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    }
+
+    render status: 200, json: { message: 'OK' }
+  end
+end
+
+module Line
+  module Bot
+    class HTTPClient
+      def http(uri)
+        proxy = URI(ENV["FIXIE_URL"])
+        http = Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
+        if uri.scheme == "https"
+          http.use_ssl = true
+        end
+
+        http
+      end
+    end
+  end
 end
