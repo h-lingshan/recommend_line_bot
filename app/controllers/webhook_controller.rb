6a1504696ee880923edc76206c94d0a79742c438
ReeceHsu <viewsoic521@gmail.com>
2017-07-09 10:02:10 +0900

    TEST

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -41,19 +41,3 @@ class WebhookController < ApplicationController
     render status: 200, json: { message: 'OK' }
   end
 end
-
-# module Line
-#   module Bot
-#     class HTTPClient
-#       def http(uri)
-#         proxy = URI(ENV["FIXIE_URL"])
-#         http = Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
-#         if uri.scheme == "https"
-#           http.use_ssl = true
-#         end
-
-#         http
-#       end
-#     end
-#   end
-# end
