28822dabdf47b6dbe31a79ebc1b118e3b410e8fc
ReeceHsu <viewsoic521@gmail.com>
2017-07-09 09:59:09 +0900

    TEST

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -42,18 +42,18 @@ class WebhookController < ApplicationController
   end
 end
 
-module Line
-  module Bot
-    class HTTPClient
-      def http(uri)
-        proxy = URI(ENV["FIXIE_URL"])
-        http = Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
-        if uri.scheme == "https"
-          http.use_ssl = true
-        end
+# module Line
+#   module Bot
+#     class HTTPClient
+#       def http(uri)
+#         proxy = URI(ENV["FIXIE_URL"])
+#         http = Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port, proxy.user, proxy.password)
+#         if uri.scheme == "https"
+#           http.use_ssl = true
+#         end
 
-        http
-      end
-    end
-  end
-end
+#         http
+#       end
+#     end
+#   end
+# end
