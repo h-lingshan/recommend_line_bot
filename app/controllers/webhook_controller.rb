005e58fa8552b79d57ec0e72111a619d9ca0ccca
ReeceHsu <viewsoic521@gmail.com>
2017-07-12 00:38:29 +0900

    test

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,5 +1,5 @@
 require 'line/bot'
-
+require 'timeout'
 class WebhookController < ApplicationController
   protect_from_forgery with: :null_session # CSRF対策無効化
 
@@ -11,8 +11,19 @@ class WebhookController < ApplicationController
   end
 
   def callback
-    body = request.body.read
+   
+    begin
+      Timeout.timeout(5) do
+        message = {
+          type: 'text',
+          text: 'こんにちは、映画サジェストです。'
+        }
+        response = client.push_message("<to>",message)
+        p response
+      end
+    end
 
+    body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     # this statement maybe mistake.
     unless client.validate_signature(body, signature)
@@ -24,17 +35,13 @@ class WebhookController < ApplicationController
       case event
       when Line::Bot::Event::Message
         case event.type
-        when Line::Bot::Event::MessageType::Text
+      when Line::Bot::Event::MessageType::Text
+          msg = event.message['text']
           message = {
             type: 'text',
             text: event.message['text']
           }
           response = client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
-        end
       end
     }
 
