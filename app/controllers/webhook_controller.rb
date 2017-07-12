9919e9299f08835ad8b5bfad64fa4835688a8ddb
lingshan.hsu <lingshan.hsu@nijibox.co.jp>
2017-07-12 21:26:00 +0900

    bug fix

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -36,7 +36,7 @@ class WebhookController < ApplicationController
         when Line::Bot::Event::Follow
         receive_follow(message_target)
         when Line::Bot::Event::MessageType::Text
-          if data_hash["context_name"].include?(event.message['text'])
+          if @data_hash["context_name"].include?(event.message['text'])
             message = {
               type: "template",
               altText: @data_hash["label"],
