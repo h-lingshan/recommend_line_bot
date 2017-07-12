9826a91e326074ad9f21af0e757ea1d838793270
lingshan.hsu <lingshan.hsu@nijibox.co.jp>
2017-07-12 21:21:54 +0900

    add question

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,6 +3,13 @@ require 'json'
 class WebhookController < ApplicationController
   protect_from_forgery with: :null_session # CSRF対策無効化
 
+  def get_sample
+   file = File.read("db/sample.json")
+    data_hash = JSON.parse(file)
+    @data_hash = data_hash
+    render :text =>  data_hash
+  end 
+
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
@@ -11,8 +18,7 @@ class WebhookController < ApplicationController
   end
 
   def callback
-    file = File.read("db/sample.js")
-    data_hash = JSON.parse(file)
+    get_sample
 
     body = request.body.read
 
@@ -31,39 +37,38 @@ class WebhookController < ApplicationController
         receive_follow(message_target)
         when Line::Bot::Event::MessageType::Text
           if event.message['text'].include?(data_hash["context_name"])
-            
+
             message = {
-              type: 'template',
-              altText: "this is a confirm template",
+              type: "template",
+              altText: @data_hash["label"],
               template: {
-                       "type": "confirm",
-                       "text": "test"
-                       "actions": [
-                          {
-                            "type": "message",
-                            "label": "Yes",
-                            "text": "yes"
-                          },
-                          {
-                            "type": "message",
-                            "label": "No",
-                            "text": "no"
-                          }
-                        ]
-               }
-          end
+                type: "confirm",
+                text: @data_hash["question"]["body"],
+                actions: [
+                  {
+                     type: "message",
+                     label: "Yes",
+                     text: "yes"
+                  },
+                  {
+                     type: "message",
+                     label: "No",
+                     text: "no"
+                  }
+                ]
+              }
+            }
+        #  end
          
           response = client.reply_message(event['replyToken'], message)
+          end
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
           tf.write(response.body)
-        end
-
-        
+        end   
       end
     }
-
     render status: 200, json: { message: 'OK' }
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   post '/callback' => 'webhook#callback'
+  get '/' => 'webhook#get_sample'
   
 end
