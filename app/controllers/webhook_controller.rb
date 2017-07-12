0c50a307a0389be68c7c041facbbb91f787c36b7
ReeceHsu <viewsoic521@gmail.com>
2017-07-12 23:34:18 +0900

    update

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -36,7 +36,6 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          if @data_hash["context_name"].include?(event.message['text'])
           message = {
            type: "template",
            altText: "this is a confirm template",
