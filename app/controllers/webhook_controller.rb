1d14bf3ba0f322e27bc4ef6c0da23873921e0969
lingshan.hsu <lingshan.hsu@nijibox.co.jp>
2017-07-13 14:27:50 +0900

    TEST

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -170,27 +170,22 @@ class WebhookController < ApplicationController
           thumbnailImageUrl: "http://realsound.jp/wp-content/uploads/2017/05/rs-jishu-01.jpg",
           title: question["label"],
           text: question["body"]["content"],
-          actions: 
+          actions:
           [
             {
-              type: "uri",
-              label: question["choice"][0]["label"],
-              uri: question["choice"][0]["content"]
+              type: "postback",
+              label: "Buy",
+              data: "action=buy&itemid=123"
+            },
+            {
+              type: "postback",
+              label: "Add to cart",
+              data: "action=add&itemid=123"
             },
             {
               type: "uri",
-              label: question["choice"][1]["label"],
-              data: question["choice"][1]["content"]
-            },
-            {
-              type: "uri",
-              label: question["choice"][2]["label"],
-              data: question["choice"][2]["content"]
-            },
-            {
-              type: "uri",
-              label: question["choice"][3]["label"],
-              data: question["choice"][3]["content"]
+              label: "View detail",
+              uri: "http://example.com/page/123"
             }
           ]
         }
