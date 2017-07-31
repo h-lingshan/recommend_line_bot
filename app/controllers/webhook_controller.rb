d56b2fd204911c4c2aeb3ddc4f391b92b0307128
lingshan.hsu <lingshan.hsu@nijibox.co.jp>
2017-07-31 16:33:09 +0900

    bug fix

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -7,9 +7,9 @@ class WebhookController < ApplicationController
  
 
   def get_sample
-    file = File.read(Dir.glob(Rails.root.join('tmp','*.json'))[0])
+    #file = File.read(Dir.glob(Rails.root.join('tmp','*.json'))[0])
    
-    data_hash = JSON.parse(file)
+    #data_hash = JSON.parse(file)
     #binding.pry
     #result=deep_find_value_with_key(data_hash,"1")
     # binding.pry
@@ -21,11 +21,11 @@ class WebhookController < ApplicationController
     
     #build_template execute_near_movietheather("222")
     #render :json => deep_find_value_with_key(data_hash,"3")
-    temp = {"events"=>[{"type"=>"postback", "replyToken"=>"e84d6e6c8b7e4abfadda336d4d5f57de", "source"=>{"userId"=>"Ubcd2b753b73e467880b4ab3f47f35d13", "type"=>"user"}, "timestamp"=>1501232128077, "postback"=>{"data"=>"id=3&parent_id=1"}}], "webhook"=>{"events"=>[{"type"=>"postback", "replyToken"=>"e84d6e6c8b7e4abfadda336d4d5f57de", "source"=>{"userId"=>"Ubcd2b753b73e467880b4ab3f47f35d13", "type"=>"user"}, "timestamp"=>1501232128077, "postback"=>{"data"=>"id=3&parent_id=1"}}]}}
-    temp_a = JSON.parse(temp.to_json)
+    #temp = {"events"=>[{"type"=>"postback", "replyToken"=>"e84d6e6c8b7e4abfadda336d4d5f57de", "source"=>{"userId"=>"Ubcd2b753b73e467880b4ab3f47f35d13", "type"=>"user"}, "timestamp"=>1501232128077, "postback"=>{"data"=>"id=3&parent_id=1"}}], "webhook"=>{"events"=>[{"type"=>"postback", "replyToken"=>"e84d6e6c8b7e4abfadda336d4d5f57de", "source"=>{"userId"=>"Ubcd2b753b73e467880b4ab3f47f35d13", "type"=>"user"}, "timestamp"=>1501232128077, "postback"=>{"data"=>"id=3&parent_id=1"}}]}}
+    #temp_a = JSON.parse(temp.to_json)
    # binding.pry
   
-    render :json =>  data_hash
+    render :text =>  "test ok"
   end 
 
   def client
