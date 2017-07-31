0756efc920a623f2cfb719d743a53e8c995f2613
lingshan.hsu <lingshan.hsu@nijibox.co.jp>
2017-07-31 15:46:50 +0900

    fixed routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'fileuploads/index'
-
-  get 'fileuploads/create'
-
-  get 'fileuploads/new'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
